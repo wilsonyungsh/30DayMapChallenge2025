46d06f5d86ada2222b05cf4241540ea2d274f4c0
wilsonyungsh <wilson0313@gmail.com>
2025-11-10 19:37:58 +1000

    feat: :sparkles: add day 4 script and map

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Placeholder for 30DayMapChallenge on 2025
 | [Day 1](https://wilsonyungsh.github.io/interactive/30DayMapChallenge2025/map1_ipswich_address_map.html) | Points   | R + duckdb + mapgl|OvertureMap|![Ipswich Suburb Address Count Map](image/map1_ipswich_address_map.jpg)|
 | Day 2 | Lines    | R + ggplot2|| |
 | [Day 3](https://wilsonyungsh.github.io/interactive/30DayMapChallenge2025/map3_address_density_map.html) | Polygons | R + duckdb + mapgl|OvertureMap| ![South East Queensland Address Density Map](image/map3_address_density_map.jpg)|
+| [Day 4](https://wilsonyungsh.github.io/interactive/30DayMapChallenge2025/Map4_Mydata_2013-2025.html) | My Data | R + Mapdeck | GoogleTimeline| ![My footprint 2013-2025](image/map4_myfootprint.jpg)|
+
--- scripts/day4_mydata.R
+++ scripts/day4_mydata.R
@@ -1,24 +1,35 @@
-pacman::p_load("tidyverse", "duckdb", "duckspatial", "sf", "mapgl", "mapdeck", "DBI", "strayr", "htmltools", "jsonlite")
+pacman::p_load("tidyverse", "sf", "mapdeck", "jsonlite")
 
+# get from device on goolge timeline output
 mydata <- fromJSON("timeline_20251109.json")
+
+# get semantic data only, covert time to timestamp
 semantic <- mydata$semanticSegments %>%
-  mutate(sd = as_date(lubridate::ymd_hms(startTime), tz = "Australia/Brisbane")) %>%
-  relocate(sd, .before = startTime)
-## fishout only visit related information - test on one day
+  mutate(sd = as_date(lubridate::ymd_hms(startTime), tz = "Australia/Brisbane"),
+    across(c(startTime, endTime), .fns = ~ ymd_hms(.x, tz = "Australia/Brisbane")))
+
+## flatten semantic
 visit <- semantic %>%
-  filter(lubridate::year(sd) == 2025) %>%
   select(sd, startTime, endTime, visit) %>% flatten() %>%
   separate(visit.topCandidate.placeLocation.latLng, into = c("lat", "lon"), sep = "°, ") %>%
-  mutate(lon = str_remove_all(string = lon, pattern = "°"),
-    duration = lubridate::ymd_hms(end, tz = "Australia/Brisbane") - starttiime) %>%
   filter(lat != "NA") %>%
-  st_as_sf(coords = c("lon", "lat"), crs = 4326)
+  mutate(lon = str_remove_all(string = lon, pattern = "°"),
+    place_type = if_else(visit.topCandidate.semanticType %in% c("INFERRED_WORK", "INFERRED_HOME", "WORK", "HOME"), "Home&Work", "Non_Home&Work"),
+    duration = round(as.numeric(endTime - startTime) / 60, 2)) %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE)
 
-visit %>%
-  mapdeck(style = mapdeck_style(style = "dark")) %>%
-  add_scatterplot(radius = 100, fill_colour = "#ffffff")
+my_data_non_homework <-
+  visit %>% select(startTime, endTime, place_type, duration, lon, lat, geometry) %>%
+  filter(place_type == "Non_Home&Work") %>%
+  mutate(across(c(lon, lat), ~ as.numeric(.x)))
 
-setwd("data/")
-con <- dbConnect(duckdb::duckdb(), dbdir = "google_timeline.duckdb", read_only = FALSE)
-dbExecute(con, "install json")
-dbExecute(con, "load json")
+my_stats <-
+  my_data_non_homework %>% st_drop_geometry() %>% mutate(date = as.Date(startTime)) %>%
+  unite("loc", c(lon, lat), sep = "|") %>%
+  group_by(date) %>% summarise(loc_cnt = n_distinct(loc), hours_outside_hw = sum(duration)) %>% arrange(desc(loc_cnt))
+
+my_data_non_homework %>%
+  mapdeck(location = c(151.08985100485512, -33.82095296781856), zoom = 9, pitch = 45) %>%
+  add_hexagon(color = "duration", color_function = "sum", radius = 500, color_opacity = 0.1, fill_opacity = 0.1,
+    elevation_scale = 20, update_view = FALSE) %>%
+  add_title("My footprint 2013-2025 from google timeline data")
